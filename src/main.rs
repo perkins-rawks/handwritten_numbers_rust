d1a9fc646fabb3f372cb295039778e57c61cd454
RockLikeBusStop <arasam1067@gmail.com>
2020-06-05 15:55:09 -0400

    testing different crates In order to find alternative for NumPy
    Co-authored-by: Awildo <agutierr@hamilton.edu>
    Co-authored-by: Sosina-abuhay <sabuhay@hamilton.edu>
    Co-authored-by: Siqi Fang <2581703212@qq.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use ndarray::prelude::*;
+use ndarray::ArrayD;
+use ndarray::IxDyn;
+
+// dot -> use ndarray::dot;
+// argmax
+// exp
+// randn
+// zeros
+
 fn main() {
-    println!("Hello, world!");
+    // let a = array![
+    //             [1.,2.,3.],
+    //             [4.,5.,6.],
+    //         ];
+    // assert_eq!(a.ndim(), 2);         // get the number of dimensions of array a
+    // assert_eq!(a.len(), 6);          // get the number of elements in array a
+    // assert_eq!(a.shape(), [2, 3]);   // get the shape of array a
+    // assert_eq!(a.is_empty(), false); // check if the array has zero elements
+
+    // println!("{:?}", a);
+    // let b = array![[[3],[3]], [[3],[4]]];
+    // let c = array![[[1],[3]], [[1], [2]]];
+    // let b: ArrayD<i32,Ix2(i0: Ix, i1: Ix)> = array![[3]],[3]];
+    // let b: ArrayD<i32,Ix2(i0: Ix, i1: Ix)> = [[3]];
+    // let a = b * c;
+    println!("{:?}", b);
 }
